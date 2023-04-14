6a5757326fb45f09a22ddb0e72d8f982b3e1f30b
jonwestman <jonwestman88@gmail.com>
2023-04-14 21:09:09 +0200

    added props to VacationList class

--- Labb1EntityFrameWork/Models/VacationList.cs
+++ Labb1EntityFrameWork/Models/VacationList.cs
@@ -1,7 +1,19 @@
-﻿namespace Labb1EntityFrameWork.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Labb1EntityFrameWork.Models
 {
     public class VacationList
     {
-
+        [Key]
+        [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.Identity)]
+        public int VacationListId { get; set; }
+        [ForeignKey(nameof(Employees))]
+        public int FK_EmployeeId { get; set; }
+        public virtual Employee Employees { get; set; }
+        [ForeignKey(nameof(Vacations))]
+        public int FK_VacationId { get; set; }
+        public virtual Vacation Vacations { get; set; }
+        
     }
 }
