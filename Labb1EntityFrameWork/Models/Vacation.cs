b93d282c710ad44039c4461b7b2551b69ddcfc10
jonwestman <jonwestman88@gmail.com>
2023-04-14 21:03:32 +0200

    updated Vacation Class

--- Labb1EntityFrameWork/Models/Vacation.cs
+++ Labb1EntityFrameWork/Models/Vacation.cs
@@ -1,7 +1,17 @@
-﻿namespace Labb1EntityFrameWork.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Labb1EntityFrameWork.Models
 {
     public class Vacation
     {
-
+        [Key]
+        [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.Identity)]
+        public int VacationId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        [Required]
+        public string VacayType { get; set; }
+        public DateTimeOffset DateApplied { get; set; } = DateTimeOffset.Now;
     }
 }
