ac9e2d79b50d96ad0bb7ee4041101586f7efe016
jonwestman <jonwestman88@gmail.com>
2023-04-14 20:53:47 +0200

    Updated VacationContext

--- Labb1EntityFrameWork/Data/VacationContext.cs
+++ Labb1EntityFrameWork/Data/VacationContext.cs
@@ -5,11 +5,16 @@ namespace Labb1EntityFrameWork.Data
 {
     public class VacationContext : DbContext
     {
-        public VacationContext()
+        public VacationContext(DbContextOptions<VacationContext> options) :base(options)
         {
             
         }
         public DbSet<Employee> Employees { get; set; }
         //public DbSet<Vacation> MyProperty { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
