3e60dcace341650963db2df3f766124e1e08020e
jonwestman <jonwestman88@gmail.com>
2023-04-17 21:37:38 +0200

    updated display names for Vacation Class

--- Labb1EntityFrameWork/Models/Vacation.cs
+++ Labb1EntityFrameWork/Models/Vacation.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Labb1EntityFrameWork.Models
@@ -9,10 +10,13 @@ namespace Labb1EntityFrameWork.Models
         [DatabaseGenerated(databaseGeneratedOption: DatabaseGeneratedOption.Identity)]
         public int VacationId { get; set; }
         [Required]
+        [DisplayName("Start date")]
         public DateOnly StartDate { get; set; }
         [Required]
+        [DisplayName("End date")]
         public DateOnly EndDate { get; set; }
         [Required]
+        [DisplayName("Reason for abscence")]
         public string VacayType { get; set; }
         public DateTime DateApplied { get; set; } = DateTime.Now;
     }
