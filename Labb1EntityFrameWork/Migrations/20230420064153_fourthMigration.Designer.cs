79053725ce08565286796f1d84f8e8dd51f6cf2c
jonwestman <jonwestman88@gmail.com>
2023-04-20 08:42:15 +0200

    fourtMigration made

--- Labb1EntityFrameWork/Migrations/20230420064153_fourthMigration.Designer.cs
+++ Labb1EntityFrameWork/Migrations/20230420064153_fourthMigration.Designer.cs
@@ -4,6 +4,7 @@ using Labb1EntityFrameWork.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Labb1EntityFrameWork.Migrations
 {
     [DbContext(typeof(VacationContext))]
-    partial class VacationContextModelSnapshot : ModelSnapshot
+    [Migration("20230420064153_fourthMigration")]
+    partial class fourthMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Labb1EntityFrameWork/Migrations/20230420064153_fourthMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Labb1EntityFrameWork.Migrations
+{
+    /// <inheritdoc />
+    public partial class fourthMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DateApplied",
+                table: "VacationLists",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateApplied",
+                table: "VacationLists");
+        }
+    }
+}
